082f94deea4c803130e46dec6ad0032701cfad56
Davranbek <davran9559@gmail.com>
2023-10-16 22:51:45 +0500

    add symbol

--- src/components/Feedback/page.jsx
+++ src/components/Feedback/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
     return (
         <section id="feedback" className="py-5">
             <div className="container mx-auto px-5">
-                <div className="flex items-center justify-center h-[556px]">
+                <div className="feedback-carousel flex items-center justify-center h-[556px]">
                     <Carousel/>
                 </div>
             </div>      
